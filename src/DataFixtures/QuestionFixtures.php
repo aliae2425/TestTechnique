77cd1ed43e9a848d74936acc441c5d9541b86d65
aliae <noam.carmi80@gmail.com>
2026-01-27 20:07:18 +0100

    feat: Update QuestionFixtures to load questions and answers from JSON; add new data structure for options

--- src/DataFixtures/QuestionFixtures.php
+++ src/DataFixtures/QuestionFixtures.php
@@ -4,7 +4,6 @@ namespace App\DataFixtures;
 
 use App\Entity\Answer;
 use App\Entity\Question;
-use DeepCopy\f001\A;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -12,39 +11,26 @@ class QuestionFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $Question = new Question();
-        $Question->setTitled('What is the capital of France?');
-        $Question->setLevel('Facile');
-        $Question->setType('Documentation');
-        $Question->setDescription('Choose the correct answer from the options below.');
-        $manager->persist($Question);
-        for ($i = 0; $i < 4; $i++) {
-            $answer = new Answer();
-            $answer->setQuestion($Question);
-            switch ($i) {
-                case 0:
-                    $answer->setText('Berlin');
-                    $answer->setIsCorrect(false);
-                    $answer->setFeedback('Berlin is the capital of Germany.');
-                    break;
-                case 1:
-                    $answer->setText('Madrid');
-                    $answer->setIsCorrect(false);
-                    $answer->setFeedback('Madrid is the capital of Spain.');
-                    break;
-                case 2:
-                    $answer->setText('Paris');
-                    $answer->setIsCorrect(true);
-                    $answer->setFeedback('Correct! Paris is the capital of France.');
-                    break;
-                case 3:
-                    $answer->setText('Rome');
-                    $answer->setIsCorrect(false);
-                    $answer->setFeedback('Rome is the capital of Italy.');
-                    break;
+        $jsonContent = file_get_contents(__DIR__ . '/data.json');
+        $data = json_decode($jsonContent, true);
+
+        foreach ($data as $item) {
+            $question = new Question();
+            $question->setTitled($item['title']);
+            $question->setLevel($item['level']);
+            $question->setType($item['type']);
+            $question->setDescription($item['Description']);
+
+            foreach ($item['options'] as $option) {
+                $answer = new Answer();
+                $answer->setText($option['label']);
+                $answer->setIsCorrect($option['isCorrect']);
+                $answer->setFeedback($option['feedback']);
+                
+                $question->addReponse($answer);
             }
-            $answer->setQuestion($Question);
-            $manager->persist($answer);
+
+            $manager->persist($question);
         }
 
         $manager->flush();
