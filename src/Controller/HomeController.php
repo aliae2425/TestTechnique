a9f90c06f055c5238214e49783cbaba8aa835e8e
aliae <noam.carmi80@gmail.com>
2026-02-02 23:37:35 +0100

    feat: add Badge component and new pages for Certifications, Formations, and Technical Tests
    - Created a new Badge component in Badge.html.twig with customizable variants.
    - Added a Certifications page with a welcome message and construction notice.
    - Added a Formations page with a welcome message and construction notice.
    - Added a Technical Tests page with a welcome message and construction notice.
    - Implemented a registration form for enterprises with validation and error handling.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,27 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'home_index')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/formations', name: 'home_formations')]
+    public function formations(): Response
+    {
+        return $this->render('home/formations.html.twig');
+    }
+
+    #[Route('/TestTechnique', name: 'home_testTechnique')]
+    public function testTechnique(): Response
+    {
+        return $this->render('home/test_technique.html.twig');
+    }
+
+    #[Route('/certification', name: 'home_certification')]
+    public function certification(): Response
+    {
+        return $this->render('home/certification.html.twig');
     }
 }
