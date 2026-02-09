9126783afd7ac735ea73751dc56edb6160f82073
aliae <noam.carmi80@gmail.com>
2026-02-09 21:58:48 +0100

    feat: update mailer configuration, adjust access control rules, and refactor user routes in UserBoardController

--- src/Controller/UserBoardController.php
+++ src/Controller/UserBoardController.php
@@ -8,10 +8,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/user')]
+#[IsGranted('ROLE_USER')]
 final class UserBoardController extends AbstractController
 {
-    #[Route('/user/board', name: 'app_user_dashboard')]
+    #[Route('/board', name: 'app_user_dashboard')]
     public function index(): Response
     {
         return $this->render('user_board/Dashboard.html.twig', [
@@ -19,7 +22,7 @@ final class UserBoardController extends AbstractController
         ]);
     }
 
-    #[Route('/user/profile', name: 'app_user_profile')]
+    #[Route('/profile', name: 'app_user_profile')]
     public function profile(Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
